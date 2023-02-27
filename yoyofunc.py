7d98f6913fb7f695b83a15dca10e6e4853460109
Bennett Korotko <Bkorotko03@gmail.com>
2023-02-27 10:12:04 -0600

    Update yoyofunc.py
    Start function loop_plot

--- yoyofunc.py
+++ yoyofunc.py
@@ -16,3 +16,11 @@ def ftare(r,m,i):
 def intertia_calc(r,m):
      i = 0.0014 + 2*(0.5*m*(0.0045**2 + r**2))
      return i
+def loop_plot():
+    r = np.arange(4.5,25.5,0.5)
+    m = 0.818 + #INSERT MASS RADIUS RELATION FOR RADIUS EXTENDERS HERE
+    i = inertia_calc(r,m)
+    a = acceleration(r,m,i)
+    f = ftare(r,m,i)
+    #PLOT RELATIONS BETWEEN FORCE,ACCELERATION AND RADIUS HEREEEEEEE
+    
