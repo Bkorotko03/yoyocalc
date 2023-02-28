542fc927575cf1917ceeb022e67aa6a9bb0926a2
Bennett Korotko <Bkorotko03@gmail.com>
2023-02-28 13:48:08 -0600

    File Dump support added
    whoopee

--- yoyocalcgui.py
+++ yoyocalcgui.py
@@ -1,5 +1,6 @@
 import yoyofunc
 import PySimpleGUI as sg
+from datetime import datetime
 def main():
     sg.theme('light grey')
     layout = [[sg.Text('Enter radius (mm) below')],
@@ -9,6 +10,8 @@ def main():
               [sg.Text(key='-OUTPUT-')],
               [sg.Button('Find Acceleration'), sg.Button('Find Moment of Inertia')], 
               [sg.Button('Find Change in Percieved Weight'), sg.Button('Print Dump to File')],
+              [sg.Text('Enter filepath for dump below')],
+              [sg.Input(key='fpath')],
               [sg.Button('Exit')]]
     window = sg.Window('PHYS126F-04E Program', layout)
     while True:
@@ -33,7 +36,17 @@ def main():
             newweight = 101.9716212978 * FF
             window['-OUTPUT-'].update(f'Percieved Change in Mass = {newweight} (g)')
         if event == 'Print Dump to File':
-            window['-OUTPUT-'].update('Coming Soon') #ADD INPUT FOR FILEPATH AND MAKE EVERYTHING NICE PLEASE
+            fpath = values['fpath']
+            rm = float(values['-RMM-'])/1000
+            mkg = 0.818 + float(values['-MG-'])/500
+            i = yoyofunc.inertia_calc(rm,mkg)
+            FF = yoyofunc.ftare(rm,mkg,i)
+            newweight = 101.9716212978 * FF
+            A = yoyofunc.acceleration(rm,mkg,i)
+            with open(f'{fpath}\yoyocalcdata.txt', 'a') as file:
+                file.write(f'\n{datetime.now()} \nMass (kg): {mkg} \nRadius (m): {rm} \nMoment of Inertia(kg*m): {i}')
+                file.write(f'\nAcceleration (m/s^2): {A} \nChange in Apparent weight (g): {newweight} \nChange in force exerted (N): {FF}')
+            window['-OUTPUT-'].update(f'File Saved at {fpath}\yoyocalcdata.txt') #ADD INPUT FOR FILEPATH AND MAKE EVERYTHING NICE PLEASE
 
     window.close()
 main()
--- yoyofunc.py
+++ yoyofunc.py
@@ -1,4 +1,4 @@
-#FUNCTIONS TO CAll IN YOYOCALC.PY
+#FUNCTIONS TO CALL IN YOYOCALC.PY
 #UNIVERSAL IMPORTS
 import numpy as np
 import scipy.constants as constant
@@ -17,14 +17,14 @@ def ftare(r,m,i):
 def inertia_calc(r,m):
      i = 0.0014 + 2*(0.5*m*(0.0045**2 + r**2))
      return i
-def loop_plot(rmm1 = 4.5, rmm2 = 25, stepmm = 0.5): #NOT SI UNITS
-    rmm2 += 0.5
+def loop_plot(rmm1 = 4.5, rmm2 = 25.5, stepmm = 0.5): #NOT SI UNITS
     rmm = np.arange(rmm1,rmm2,stepmm)
     r = rmm/1000
     m = 0.818 #REMEMBER TO ADD MASS RADIUS RELATION FOR RADIUS EXTENDER, ALSO MULT BY 2 BECAUSE THERES 2 RAD EXTENDERS
     i = inertia_calc(r,m)
     a = acceleration(r,m,i)
     f = ftare(r,m,i)
-    aceelgraph = plt.plot(a,rmm)
-    plt.savefig()
+    accelgraph = plt.plot(a,rmm)
+    fgraph = plt.plot(f,rmm)
+    plt.show()
     
